cf49bb2b7af62af843525d45384168926395c9f7
Abimanyu <abimanyu.damarjati@gmail.com>
2022-11-24 11:19:34 +0700

    Test Controller

--- src/main/java/com/enjoy/kanjurbackend/model/User.java
+++ src/main/java/com/enjoy/kanjurbackend/model/User.java
@@ -1,26 +1,33 @@
 package com.enjoy.kanjurbackend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
 @Table(name = "users")
 public class User {
-    private int studentId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "student_id", nullable = false)
+    private Long studentId;
+
     private String name;
     private double revenue, deposit, withdraw, debt;
     private boolean flag;
     private Date createdAt, updatedAt, deletedAt;
 
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
     public User() {}
 
-    public User(int studentId, String name) {
+    public User(Long studentId, String name) {
         this.studentId = studentId;
         this.name = name;
     }
 
-    public int getStudentId() {
+    public Long getStudentId() {
         return studentId;
     }
 }
